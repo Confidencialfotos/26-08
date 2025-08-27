e2ba12a0e8233d5e4b13bfcd3a06a4898ae425c7
Confidencialfotos <betocunhafotos@gmail.com>
2025-08-27 19:25:21 -0300

    27/09

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,9 +1,8 @@
 console.log(document.body);
-
 console.log(document.body.childNodes);
-
 console.log(document.body.childNodes[1]);
-
 console.log(document.body.childNodes[1].childNodes);
-
 console.log(document.body.childNodes[1].childNodes[1].textContent);
+//Selecionar por TAG
+const listaItens = document.getElementsByTagName('li');
+console.log(listaItens);
