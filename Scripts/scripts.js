c8f179bad7f7e75e862a1140a10f56b104d99ffd
Confidencialfotos <betocunhafotos@gmail.com>
2025-08-27 20:32:21 -0300

    teste

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -5,4 +5,8 @@ console.log(document.body.childNodes[1].childNodes);
 console.log(document.body.childNodes[1].childNodes[1].textContent);
 //Selecionar por TAG
 const listaItens = document.getElementsByTagName('li');
-console.log(listaItens);
+console.log(listaItens);,
+const botao = document.getElementById('btn');
+botao.addEventListener('click', function(){
+   alert("voce clicou no botao!") 
+});
