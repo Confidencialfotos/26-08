704227476e021017fa1f0c314086f609bcd5cbaf
Confidencialfotos <betocunhafotos@gmail.com>
2025-08-27 22:47:16 -0300

    Update scripts.js

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,17 +1,17 @@
-console.log(document.body);
-console.log(document.body.childNodes);
-console.log(document.body.childNodes[1]);
-console.log(document.body.childNodes[1].childNodes);
-console.log(document.body.childNodes[1].childNodes[1].textContent);
-//Selecionar por TAG
-const listaItens = document.getElementsByTagName('li');
-console.log(listaItens);,
+// console.log(document.body);
+// console.log(document.body.childNodes);
+// console.log(document.body.childNodes[1]);
+// console.log(document.body.childNodes[1].childNodes);
+// console.log(document.body.childNodes[1].childNodes[1].textContent);
+// //Selecionar por TAG
+// const listaItens = document.getElementsByTagName('li');
+// console.log(listaItens);
 // const botao = document.getElementById('btn');
 // botao.addEventListener('click', function() {
 //    alert("voce clicou no botao!") 
 // });
 const cor=document.getElementById('btn');
-cor.addEventListener('Click', function(){
+cor.addEventListener('click', function(){
     document.body.style.backgroundColor = 'black'
 });
 
